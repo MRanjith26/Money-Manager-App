34de8db9850632767e566f50517acbc3f81db1ee
MRanjith26 <mranjith.2609@gmail.com>
2024-01-13 14:40:32 +0000

    added functionality

--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -1,6 +1,8 @@
 import './index.css'
 import {Component} from 'react'
+import {v4} from 'uuid'
 import MoneyDetails from '../MoneyDetails/index'
+import TransactionItem from '../TransactionItem/index'
 
 const transactionTypeOptions = [
   {
@@ -15,10 +17,106 @@ const transactionTypeOptions = [
 
 // Write your code here
 class MoneyManager extends Component {
-  state = {Balance: 0, Income: 0, Expenses: 0}
+  state = {
+    Balance: 0,
+    Income: 0,
+    Expenses: 0,
+    historyList: [],
+    Title: '',
+    Amount: '',
+    optionId: transactionTypeOptions[0].optionId,
+  }
+
+  getActiveDeleteID = (id, Amount, Type) => {
+    this.setState(prevState => ({
+      historyList: prevState.historyList.filter(
+        eachHistory => eachHistory.id !== id,
+      ),
+    }))
+
+    if (Type === 'Expenses') {
+      this.setState(prevState => ({
+        Balance: prevState.Balance + parseInt(Amount),
+      }))
+      this.setState(prevState => ({
+        Expenses: prevState.Expenses - Amount,
+      }))
+    } else if (Type === 'Income') {
+      this.setState(prevState => ({
+        Income: prevState.Income - Amount,
+      }))
+      this.setState(prevState => ({
+        Balance: prevState.Balance - Amount,
+      }))
+    }
+  }
+
+  onAddHistoryDetails = event => {
+    event.preventDefault()
+    const {Title, Amount, optionId} = this.state
+
+    const findType = transactionTypeOptions.filter(
+      eachTransaction => eachTransaction.optionId === optionId,
+    )
+
+    const newHistory = {
+      id: v4(),
+      Title,
+      Amount,
+      Type: findType[0].displayText,
+    }
+    if (newHistory.Type === 'Income') {
+      this.setState(prevState => ({
+        Balance: prevState.Balance + parseInt(Amount),
+      }))
+      this.setState(prevState => ({
+        Income: prevState.Income + parseInt(Amount),
+      }))
+    } else if (newHistory.Type === 'Expenses') {
+      this.setState(prevState => ({
+        Balance: prevState.Balance - parseInt(Amount),
+      }))
+      this.setState(prevState => ({
+        Expenses: prevState.Expenses + parseInt(Amount),
+      }))
+    }
+
+    this.setState(prevState => ({
+      historyList: [...prevState.historyList, newHistory],
+      Title: '',
+      Amount: '',
+      optionId: transactionTypeOptions[0].optionId,
+    }))
+  }
+
+  onAddTitle = event => {
+    this.setState({
+      Title: event.target.value,
+    })
+  }
+
+  onAddAmount = event => {
+    this.setState({
+      Amount: event.target.value,
+    })
+  }
+
+  onAddType = event => {
+    this.setState({
+      optionId: event.target.value,
+    })
+  }
 
   render() {
-    const {Balance, Income, Expenses} = this.state
+    const {
+      Balance,
+      Income,
+      Expenses,
+      Title,
+      Amount,
+      optionId,
+      historyList,
+    } = this.state
     return (
       <div className="bg-container">
         <div className="container">
@@ -38,7 +136,10 @@ class MoneyManager extends Component {
           />
           <div className="data-container">
             <div className="form-container">
-              <form className="form-details">
+              <form
+                className="form-details"
+                onSubmit={this.onAddHistoryDetails}
+              >
                 <h1 className="data-title">Add Transaction</h1>
                 <label htmlFor="text">TITLE</label>
                 <input
@@ -46,6 +147,8 @@ class MoneyManager extends Component {
                   id="text"
                   placeholder="TITLE"
                   className="form-input"
+                  value={Title}
+                  onChange={this.onAddTitle}
                 />
                 <label htmlFor="amount">AMOUNT</label>
                 <input
@@ -53,12 +156,20 @@ class MoneyManager extends Component {
                   id="amount"
                   placeholder="AMOUNT"
                   className="form-input"
+                  value={Amount}
+                  onChange={this.onAddAmount}
                 />
                 <label htmlFor="value">TYPE</label>
-                <select name="type" id="value" className="form-input">
+                <select
+                  name="type"
+                  id="value"
+                  className="form-input"
+                  value={optionId}
+                  onChange={this.onAddType}
+                >
                   {transactionTypeOptions.map(eachType => (
                     <option
-                      value={eachType.displayText}
+                      value={eachType.optionId}
                       className="option-value"
                       key={eachType.optionId}
                     >
@@ -80,6 +191,13 @@ class MoneyManager extends Component {
                     <p className="history-title">Amount</p>
                     <p className="history-title">Type</p>
                   </div>
+                  {historyList.map(eachHistory => (
+                    <TransactionItem
+                      historyDetails={eachHistory}
+                      key={eachHistory.id}
+                      getActiveDeleteID={this.getActiveDeleteID}
+                    />
+                  ))}
                 </ul>
               </div>
             </div>
--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -1,1 +1,32 @@
 // Write your code here
+import './index.css'
+
+const TransactionItem = props => {
+  const {historyDetails, getActiveDeleteID} = props
+  const {id, Title, Amount, Type} = historyDetails
+
+  const onDelete = () => {
+    getActiveDeleteID(id, Amount, Type)
+  }
+
+  return (
+    <li className="History-Item">
+      <p className="history-text">{Title}</p>
+      <p className="history-text">Rs {Amount}</p>
+      <p className="history-text">{Type}</p>
+      <button
+        className="delete-btn"
+        data-testid="delete"
+        type="button"
+        onClick={onDelete}
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+          alt="delete"
+          className="delete-icon"
+        />
+      </button>
+    </li>
+  )
+}
+export default TransactionItem
